be54097c09d529dee6b3eef61da0f0b0510cdadc
Romi Julianto <romyjulians@gmail.com>
2025-02-25 11:43:52 +0700

    feat: add initial widget implementation with configuration and testing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,32 @@
 module.exports = {
-    apps: [{
-      name: "exb-client",
-      script: "./node_modules/webpack/bin/webpack.js",
-      args: "--mode development --watch",
-      cwd: "./",
-      env: {
-        NODE_ENV: "development"
-      },
-      watch: false,
-      instances: 1,
-      autorestart: true,
-      max_memory_restart: "1G",
-      interpreter: "node"
-    }]
+  apps: [{
+    name: "exb-client",
+    script: "./node_modules/webpack/bin/webpack.js",
+    args: "--mode development --watch",
+    cwd: "./",
+    env: {
+      NODE_ENV: "development"
+    },
+    watch: true,
+    watch_options: {
+      followSymlinks: false,
+      usePolling: true,
+      interval: 100, 
+      binaryInterval: 100, 
+      paths: ["your-extensions"],
+      awaitWriteFinish: {
+        stabilityThreshold: 50, 
+        pollInterval: 100 
+      }
+    },
+    ignore_watch: [
+      "node_modules",
+      "dist",
+      "*.log"
+    ],
+    instances: 1,
+    autorestart: true,
+    max_memory_restart: "1G",
+    interpreter: "node"
+  }]
 };
